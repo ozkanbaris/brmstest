17790df457d0b9b575d267b9f7c016ad13e0b14b
ozkanbaris <javaceylan@gmail.com>
2019-06-04 09:57:59 +0200

    lan

--- MultiLev.R
+++ MultiLev.R
@@ -21,4 +21,17 @@ b12.2 <-
       iter = 4000, warmup = 1000, chains = 4, cores = 4,
       seed = 12)
 b12.2 <- add_criterion(b12.2, "waic")
+plot(b12.2)
+
 w <- loo_compare(b12.2, criterion = "waic")
+
+
+dotR <- file.path(Sys.getenv("HOME"), ".R")
+if (!file.exists(dotR)) dir.create(dotR)
+M <- file.path(dotR, ifelse(.Platform$OS.type == "windows", "Makevars.win", "Makevars"))
+if (!file.exists(M)) file.create(M)
+cat("\nCXX14FLAGS=-O3 -march=native -mtune=native",
+    if( grepl("^darwin", R.version$os)) "CXX14FLAGS += -arch x86_64 -ftemplate-depth-256" else 
+      if (.Platform$OS.type == "windows") "CXX11FLAGS=-O3 -march=native -mtune=native" else
+        "CXX14FLAGS += -fPIC",
+    file = M, sep = "\n", append = TRUE)
