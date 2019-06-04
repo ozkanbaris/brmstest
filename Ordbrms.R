263073b3eb87b0b4e141205b1b13cfc3a356f2c7
ozkanbaris <javaceylan@gmail.com>
2019-06-04 19:02:46 +0200

    ord

--- Ordbrms.R
+++ Ordbrms.R
@@ -1,25 +1,35 @@
+# 
+# 
+# library("brms")
+# library("tidyverse")
+# library("ordPens")
+# data("ICFCoreSetCWP")
+# data(Howell1)
+# cwp <- ICFCoreSetCWP
+# prior1 <- prior(normal(0, 10), class = "b") +
+#   prior(dirichlet(1, 1, 1), class = "simo", coef = "mod4501") +
+#   prior(dirichlet(1, 1, 1, 1), class = "simo", coef = "mod4551")
+# fit1 <- brm(phcs ~ mo(d450) + mo(d455), data = cwp, prior = prior1)
+
 library (tidyverse)
 library (brms)
 
-a<-c("1","2")
-b<-c("1","2","3")
-rat1<-factor(sample(a,50,TRUE))
-rat2<-factor(sample(b,50,TRUE))
+a<-c("1","2","3")
+rat1<-factor(sample(a,50,TRUE,c(1/3,1/3,1/3)))
+rat2<-factor(sample(a,50,TRUE,c(1/5,2/5,2/5)))
+rating<-c(rat1,rat2)
 
 subject<-rep(seq(1,50),2)
-rating<-c(rat1,rat2)
-rep(seq(1,50),2)
+ab <- factor(c(rep(1,50),rep(2,50)), ordered = FALSE)
 
-ab_options<-c("1","2")
-ab <- factor(c(seq(1,50),seq(2,50)), ordered = FALSE)
+prior_ma <- prior(normal(0, 5), class = "b") +
+  prior(normal(0, 5), class = "Intercept")
 
 dat <- data.frame(rating, ab,subject)
 
-mean_ls <- c(30, 60, 70, 75)
-ls <- mean_ls[income] + rnorm(100, sd = 7)
 
-
-fit1 <- brm(ls ~ mo(income)+ab+(1|subject), data = dat)
+fit1 <- brm(rating ~ ab+(1|subject), data = dat, prior= prior_ma, family=acat(link= "logit", threshold="flexible"))
 summary(fit1)
-plot(fit1, pars = "simo")
-plot(marginal_effects(fit1))
+plot(fit1)
+plot(marginal_effects(fit1, categorical = TRUE))
+
